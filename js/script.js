5cc4f875a1ab20b58a32638987bf9e649d86543c
Luis Fimbres G <2016.lafg@gmail.com>
2025-11-03 14:26:17 -0600

    Avance (11-3-2025)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 window.onload = () => {
 
+    arrReview = []
+    if(localStorage.getItem("reviews")){
+        arrReview = JSON.parse(localStorage.getItem("reviews"))
+        document.getElementById("reviews-container").innerHTML = `
+            <div class="review">
+            <h3>Great product</h3>
+            <p>I think this product is great, I think it is the best out there!</p>
+            </div>
+        `
+
+        for (let i = 0; i < arrReview.length; i++){
+            document.getElementById("reviews-container").innerHTML += `
+            <div class="review">
+                <h3>${arrReview[i].title}</h3>
+                <p>${arrReview[i].content}</p>
+            </div>
+            `
+        }
+    }
+
     let sizes = document.querySelector(".sizes")
     let lb_current_price = document.querySelector(".current")
     let lb_old_price = document.querySelector(".old")
     let lb_discount_price = document.querySelector(".discount")
+    let currentPriceLocal = 0
+    let total = document.getElementById("total")
 
     lb_old_price.classList.add('noDisplay')
     lb_discount_price.classList.add('noDisplay')
@@ -17,6 +39,8 @@ window.onload = () => {
         if(i == arr.length-1){
             sizes.innerHTML += (`<button class="size-btn active">${arr[i].size}</button>`)
             lb_current_price.textContent = "$"+arr[i].price
+            currentPriceLocal = arr[i].price
+            total.textContent = "Total: $"+(parseFloat(lb_current_price.textContent.replace('$',""))).toFixed(2)
         }
         else{
             sizes.innerHTML += (`<button class="size-btn">${arr[i].size}</button>`)
@@ -68,9 +92,19 @@ window.onload = () => {
                 item.classList.remove('active')
             })
             evt.target.classList.add('active')
-            if (evt.target == sizeML[i]){
+            if (evt.target.target == sizeML[i].target){
                 lb_current_price.textContent = "$"+arr[i].price
+                currentPriceLocal = arr[i].price
             }
+            lb_discount_price.classList.add("noDisplay")
+            lb_old_price.classList.add("noDisplay")
+            inputQuantity.value = 1
+            if (inputQuantity == ''){
+                total.textContent = ("--")
+            }
+            else (
+                total.textContent = "Total: $"+(parseFloat(lb_current_price.textContent.replace('$',""))*parseInt(inputQuantity.value)).toFixed(2)
+            )
         })
     }
 
@@ -89,24 +123,37 @@ window.onload = () => {
             inputQuantity.value = ''
         }
 
-        if (inputQuantity.value > 5){
-            let currentButton = document.getElementsByClassName("size-btn active")
-
-            let selection = 0
-
-            for (let i = 0; i < sizeML.length; i++){
-                console.log("LOOP")
-                if(sizeML[i] == currentButton){
-                    selection = "TEST"
-                    console.log(selection)
-                }
-            }
-
-            let originalPrice = parseFloat(lb_current_price.textContent.replace('$',''))
-            lb_current_price.textContent = "$"+(originalPrice - (originalPrice*.10)).toFixed(2)
-            console.log(lb_current_price.textContent)
-            
+        if (inputQuantity.value <= 5){
+            lb_current_price.textContent = "$"+parseFloat(currentPriceLocal).toFixed(2)
+            lb_discount_price.classList.add("noDisplay")
+            lb_old_price.classList.add("noDisplay")
         }
+
+        if (inputQuantity.value > 5){
+            lb_current_price.textContent = "$"+(currentPriceLocal - (currentPriceLocal*.10)).toFixed(2)
+            lb_old_price.textContent = "$"+currentPriceLocal
+            lb_discount_price.textContent = "10% OFF"
+            lb_old_price.classList.remove("noDisplay")
+            lb_discount_price.classList.remove("noDisplay")
+        }
+
+        if (inputQuantity.value > 10){
+            lb_current_price.textContent = "$"+(currentPriceLocal - (currentPriceLocal*.20)).toFixed(2)
+            lb_old_price.textContent = "$"+currentPriceLocal
+            lb_discount_price.textContent = "20% OFF"
+            lb_old_price.classList.remove("noDisplay")
+            lb_discount_price.classList.remove("noDisplay")
+        }
+
+        if (inputQuantity == ''){
+            total.textContent = ("--")
+        }
+        else if(isNaN(inputValue)){
+            total.textContent = "Total: - - - - -"
+        }
+        else (
+            total.textContent = "Total: $"+(parseFloat(lb_current_price.textContent.replace('$',""))*parseInt(inputQuantity.value)).toFixed(2)
+        )
     })
     inputQuantity.addEventListener('keydown', (evt) =>{
         if (evt.key == '.' || evt.key == '-' || evt.key == '+' || evt.key == 'e' || evt.key == 'E' || evt.key == '*' || evt.key == '/'){
@@ -121,6 +168,31 @@ window.onload = () => {
         if (isNaN(parseInt(inputQuantity.value))){
             inputQuantity.value = 1
         }
+        if (inputQuantity.value <= 5){
+            lb_current_price.textContent = "$"+parseFloat(currentPriceLocal).toFixed(2)
+            lb_discount_price.classList.add("noDisplay")
+            lb_old_price.classList.add("noDisplay")
+        }
+        if (inputQuantity.value > 5){
+            lb_current_price.textContent = "$"+(currentPriceLocal - (currentPriceLocal*.10)).toFixed(2)
+            lb_old_price.textContent = "$"+currentPriceLocal
+            lb_discount_price.textContent = "10% OFF"
+            lb_old_price.classList.remove("noDisplay")
+            lb_discount_price.classList.remove("noDisplay")
+        }
+        if (inputQuantity.value > 10){
+            lb_current_price.textContent = "$"+(currentPriceLocal - (currentPriceLocal*.20)).toFixed(2)
+            lb_old_price.textContent = "$"+currentPriceLocal
+            lb_discount_price.textContent = "20% OFF"
+            lb_old_price.classList.remove("noDisplay")
+            lb_discount_price.classList.remove("noDisplay")
+        }
+        if (inputQuantity == ''){
+            total.textContent = ("--")
+        }
+        else (
+            total.textContent = "Total: $"+(parseFloat(lb_current_price.textContent.replace('$',""))*parseInt(inputQuantity.value)).toFixed(2)
+        )
     })
 
     btnDecrease.addEventListener('click', () =>{
@@ -130,5 +202,50 @@ window.onload = () => {
         if (isNaN(parseInt(inputQuantity.value))){
             inputQuantity.value = 1
         }
+        if (inputQuantity.value <= 5){
+            lb_current_price.textContent = "$"+parseFloat(currentPriceLocal).toFixed(2)
+            lb_discount_price.classList.add("noDisplay")
+            lb_old_price.classList.add("noDisplay")
+        }
+        if (inputQuantity.value > 5){
+            lb_current_price.textContent = "$"+(currentPriceLocal - (currentPriceLocal*.10)).toFixed(2)
+            lb_old_price.textContent = "$"+currentPriceLocal
+            lb_discount_price.textContent = "10% OFF"
+            lb_old_price.classList.remove("noDisplay")
+            lb_discount_price.classList.remove("noDisplay")
+        }
+        if (inputQuantity.value > 10){
+            lb_current_price.textContent = "$"+(currentPriceLocal - (currentPriceLocal*.20)).toFixed(2)
+            lb_old_price.textContent = "$"+currentPriceLocal
+            lb_discount_price.textContent = "20% OFF"
+            lb_old_price.classList.remove("noDisplay")
+            lb_discount_price.classList.remove("noDisplay")
+        }
+        if (inputQuantity == ''){
+            total.textContent = ("--")
+        }
+        else (
+            total.textContent = "Total: $"+(parseFloat(lb_current_price.textContent.replace('$',""))*parseInt(inputQuantity.value)).toFixed(2)
+        )
     })
+}
+
+let inputTitle = document.getElementById("inputReviewTitle")
+let inputContent = document.getElementById("inputReviewContent")
+
+
+
+function addReview() {
+    if(inputTitle.value != "" || inputContent.value != ""){
+        document.getElementById("reviews-container").innerHTML += `
+        <div class="review">
+          <h3>${inputTitle.value}</h3>
+          <p>${inputContent.value}</p>
+        </div>
+        `
+        arrReview.push({title: inputTitle.value, content:inputContent.value})
+        console.log(arrReview[0])
+
+        localStorage.setItem("reviews",JSON.stringify(arrReview))
+    }
 }
